fef4122fd55bc4c32587197009fbfc2398329d38
Karim Ali <kareemaly.2004@gmail.com>
2025-10-15 23:41:00 +0300

    smtp-email-service is built

--- src/MetaBlog.Infrastructure/Services/SmtpEmailService.cs
+++ src/MetaBlog.Infrastructure/Services/SmtpEmailService.cs
@@ -1,10 +1,13 @@
 ﻿using MetaBlog.Domain.Common.Results;
 using MetaBlog.Infrastructure.Interfaces;
 using MetaBlog.Infrastructure.Settings;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,13 +15,40 @@ namespace MetaBlog.Infrastructure.Services
 {
     public class SmtpEmailService : IEmailService
     {
-        public SmtpEmailService(IOptions<EmailSettings>emailSettings) { 
+        public SmtpEmailService(IOptions<EmailSettings>emailSettings,ILogger<SmtpEmailService>logger) { 
             _emailSettings=emailSettings.Value;
+            _logger = logger;
         }
         private readonly EmailSettings _emailSettings;
-        public Task<Result<Success>> SendAsync(string toEmail,string Subject,string Message)
+        private readonly ILogger<SmtpEmailService> _logger; 
+        public async Task<Result<Success>> SendAsync(string toEmail,string Subject,string Message)
         {
-            throw new NotImplementedException();
+            using var client = new SmtpClient(_emailSettings.SmtpServer, _emailSettings.SmtpPort)
+            {
+                Credentials = new NetworkCredential(_emailSettings.SmtpUser, _emailSettings.SmtpPass),
+                EnableSsl = true
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(_emailSettings.FromEmail, "MetaBlog Support"),
+                Subject = Subject,
+                Body = Message,
+                IsBodyHtml = false
+            };
+
+            mailMessage.To.Add(toEmail);
+            try
+            {
+
+                await client.SendMailAsync(mailMessage);
+            }
+            catch (Exception ex) {
+                _logger.LogError(ex,"smtp server failure in sending mail to {email}.", toEmail);
+                return Error.Failure();
+            }
+
+            return Result.Success;
         }
     }
 }
